f41b317046c68730a93e02303f5b0bb8ece8e8b5
iTecAI <matteovh@gmail.com>
2021-07-07 08:09:05 -0400

    comments, encryption on GET request responses

--- client.py
+++ client.py
@@ -10,32 +10,58 @@ class Connection:
         self.addr = (host, port)
         self.protocol = protocol
         self.public, self.private = rsa.newkeys(512)
-        self.server_public = rsa.PublicKey.load_pkcs1(
+        self.server_public = rsa.PublicKey.load_pkcs1( # Get public key from server
             base64.urlsafe_b64decode(
                 requests.get(f'{self.protocol}://{self.addr[0]}:{str(self.addr[1])}')
                     .headers['X-Public-Key']
             )
         )
-    def _request(self, method, path, data):
-        f_key = Fernet.generate_key()
-        fernet = Fernet(f_key)
-        resp = requests.request(str(method).upper(), f'{self.protocol}://{self.addr[0]}:{str(self.addr[1])}{path}', data=json.dumps({
-            'key': base64.urlsafe_b64encode(rsa.encrypt(f_key, self.server_public)).decode('utf-8'),
-            'data': base64.urlsafe_b64encode(fernet.encrypt(json.dumps(data).encode('utf-8'))).decode('utf-8')
-        }), headers={'X-Public-Key': base64.urlsafe_b64encode(self.public.save_pkcs1()).decode('utf-8')})
-        self.server_public = rsa.PublicKey.load_pkcs1(
-            base64.urlsafe_b64decode(
-                resp.headers['X-Public-Key']
+    def _request(self, method, path, data={}): # Basic request function
+        if method == 'POST': # Encrypt POST requests
+            f_key = Fernet.generate_key() # Generate temporary symmetric encryption key
+            fernet = Fernet(f_key)
+            resp = requests.request(str(method).upper(), f'{self.protocol}://{self.addr[0]}:{str(self.addr[1])}{path}', data=json.dumps({
+                'key': base64.urlsafe_b64encode(rsa.encrypt(f_key, self.server_public)).decode('utf-8'), # RSA-encrypted Fernet key
+                'data': base64.urlsafe_b64encode(fernet.encrypt(json.dumps(data).encode('utf-8'))).decode('utf-8') # Fernet-encrypted data
+            }), headers={'X-Public-Key': base64.urlsafe_b64encode(self.public.save_pkcs1()).decode('utf-8')})
+            self.server_public = rsa.PublicKey.load_pkcs1( # Update server public key
+                base64.urlsafe_b64decode(
+                    resp.headers['X-Public-Key']
+                )
             )
-        )
-        try:
-            raw_json = resp.json()
-        except JSONDecodeError:
-            raise ConnectionError(f'Recieved a non-JSON response from the CHAD server: "{str(resp.text)}" with status code {str(resp.status_code)}.')
-        encrypted_data = base64.urlsafe_b64decode(raw_json['data'].encode('utf-8'))
-        encryption_key = Fernet(rsa.decrypt(base64.urlsafe_b64decode(raw_json['key'].encode('utf-8')), self.private))
-        decrypted_data = encryption_key.decrypt(encrypted_data).decode('utf-8')
-        try:
-            return json.loads(decrypted_data)
-        except JSONDecodeError:
-            return decrypted_data
+            # Decrypt recieved response
+            try:
+                raw_json = resp.json()
+            except JSONDecodeError:
+                raise ConnectionError(f'Recieved a non-JSON response from the CHAD server: "{str(resp.text)}" with status code {str(resp.status_code)}.')
+            encrypted_data = base64.urlsafe_b64decode(raw_json['data'].encode('utf-8'))
+            encryption_key = Fernet(rsa.decrypt(base64.urlsafe_b64decode(raw_json['key'].encode('utf-8')), self.private)) # get Fernet key
+            decrypted_data = encryption_key.decrypt(encrypted_data).decode('utf-8') # Decrypt data
+            # Return as JSON if possible, otherwise return raw data as string
+            try:
+                return json.loads(decrypted_data)
+            except JSONDecodeError:
+                return decrypted_data
+        else: # Do not encrypt GET requests, but decrypt response body
+            resp = requests.request(
+                str(method).upper(), f'{self.protocol}://{self.addr[0]}:{str(self.addr[1])}{path}', 
+                headers={'x-public-key': base64.urlsafe_b64encode(self.public.save_pkcs1()).decode('utf-8')},
+                params=data
+            )
+            # Decrypt recieved response
+            try:
+                raw_json = resp.json()
+            except JSONDecodeError:
+                raise ConnectionError(f'Recieved a non-JSON response from the CHAD server: "{str(resp.text)}" with status code {str(resp.status_code)}.')
+            encrypted_data = base64.urlsafe_b64decode(raw_json['data'].encode('utf-8'))
+            encryption_key = Fernet(rsa.decrypt(base64.urlsafe_b64decode(raw_json['key'].encode('utf-8')), self.private)) # get Fernet key
+            decrypted_data = encryption_key.decrypt(encrypted_data).decode('utf-8') # Decrypt data
+            # Return as JSON if possible, otherwise return raw data as string
+            try:
+                return json.loads(decrypted_data)
+            except JSONDecodeError:
+                return decrypted_data
+
+conn = Connection('localhost', 1500)
+print(conn._request('POST', '/', {'test':'test'}))
+print(conn._request('GET', '/', {'test':'test'}))
--- server.py
+++ server.py
@@ -15,28 +15,28 @@ from util import *
 
 app = FastAPI()
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Get args on program initial load
     parser = ArgumentParser(description='Run the CHAD server.')
     parser.add_argument('--config', default='config.json', help='Path to the CHAD configuration JSON file. Defaults to config.json.')
 
     args = parser.parse_args()
     with open(args.config, 'r') as f:
         CONFIG = json.load(f)
-    os.environ['CHAD-CONFIGURATION'] = args.config
-else:
+    os.environ['CHAD-CONFIGURATION'] = args.config # Pass config path to ENV for uvicorn run
+else: # Run when running through uvicorn
     try:
         with open(os.environ['CHAD-CONFIGURATION'], 'r') as f:
-            CONFIG = json.load(f)
+            CONFIG = json.load(f) # Load configuration
     except FileNotFoundError:
         raise ValueError('Bad value for the CHAD configuration path passed to ENVVARS')
     except KeyError:
         raise RuntimeError('The CHAD server is not meant to act as a module.')
-    if os.path.exists(default(CONFIG, 'privateKey', 'privKey.pem')) and os.path.exists(default(CONFIG, 'publicKey', 'pubKey.pem')):
+    if os.path.exists(default(CONFIG, 'privateKey', 'privKey.pem')) and os.path.exists(default(CONFIG, 'publicKey', 'pubKey.pem')): # Load saved RSA keys
         with open(default(CONFIG, 'privateKey', 'privKey.pem'), 'rb') as prk:
             PRIVATE_KEY = rsa.PrivateKey.load_pkcs1(prk.read())
         with open(default(CONFIG, 'publicKey', 'pubKey.pem'), 'rb') as pbk:
             PUBLIC_KEY = rsa.PublicKey.load_pkcs1(pbk.read())
-    else:
+    else: # Generate and save new RSA keys
         PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(512)
         with open(default(CONFIG, 'privateKey', 'privKey.pem'), 'wb') as prk:
             prk.write(PRIVATE_KEY.save_pkcs1())
@@ -44,27 +44,27 @@ else:
             pbk.write(PUBLIC_KEY.save_pkcs1())
 
 @app.middleware('http')
-async def process_request(request: Request, call_next):
+async def process_request(request: Request, call_next): # decrypt POST requests
     if request.method.lower() == 'post':
-        client_public_key = rsa.PublicKey.load_pkcs1(base64.urlsafe_b64decode(request.headers['X-Public-Key'].encode('utf-8')))
+        client_public_key = rsa.PublicKey.load_pkcs1(base64.urlsafe_b64decode(request.headers['X-Public-Key'].encode('utf-8'))) # Get client public key from headers
         raw_body_json = await request.json()
-        encrypted_data = base64.urlsafe_b64decode(raw_body_json['data'].encode('utf-8'))
-        encryption_key = Fernet(rsa.decrypt(base64.urlsafe_b64decode(raw_body_json['key'].encode('utf-8')), PRIVATE_KEY))
-        decrypted_data = json.loads(encryption_key.decrypt(encrypted_data).decode('utf-8'))
+        encrypted_data = base64.urlsafe_b64decode(raw_body_json['data'].encode('utf-8')) # Get encrypted data
+        encryption_key = Fernet(rsa.decrypt(base64.urlsafe_b64decode(raw_body_json['key'].encode('utf-8')), PRIVATE_KEY)) # Get RSA-encrypted Fernet key
+        decrypted_data = json.loads(encryption_key.decrypt(encrypted_data).decode('utf-8')) # Decrypt data
         request.state.data = decrypted_data
 
-        raw_response: StreamingResponse = await call_next(request)
+        raw_response: StreamingResponse = await call_next(request) # Call endpoint
         
 
         body = b""
-        async for chunk in raw_response.body_iterator:
+        async for chunk in raw_response.body_iterator: # Get response body
             body += chunk
         
-        new_key = Fernet.generate_key()
-        encrypted_body = base64.urlsafe_b64encode(Fernet(new_key).encrypt(body)).decode('utf-8')
-        encrypted_key = base64.urlsafe_b64encode(rsa.encrypt(new_key, client_public_key)).decode('utf-8')
+        new_key = Fernet.generate_key() # Generate new key
+        encrypted_body = base64.urlsafe_b64encode(Fernet(new_key).encrypt(body)).decode('utf-8') # Encrypt body
+        encrypted_key = base64.urlsafe_b64encode(rsa.encrypt(new_key, client_public_key)).decode('utf-8') # Encrypt Fernet key
         
-        final =  Response(
+        final = Response( # Assemble new response object
             content=json.dumps({
                 'key': encrypted_key,
                 'data': encrypted_body
@@ -72,20 +72,38 @@ async def process_request(request: Request, call_next):
             status_code=raw_response.status_code,
             media_type=raw_response.media_type
         )
-        final.headers['X-Public-Key'] = base64.urlsafe_b64encode(PUBLIC_KEY.save_pkcs1()).decode('utf-8')
+        final.headers['X-Public-Key'] = base64.urlsafe_b64encode(PUBLIC_KEY.save_pkcs1()).decode('utf-8') # Set public key header
         return final
-    else:
+    else: # Do not process GET requests
         resp: Response = await call_next(request)
-        resp.headers['X-Public-Key'] = base64.urlsafe_b64encode(PUBLIC_KEY.save_pkcs1()).decode('utf-8')
+        print(request.headers.keys())
+        if 'x-public-key' in request.headers.keys(): # Encrypt return data if the request headers include a public key
+            client_public_key = rsa.PublicKey.load_pkcs1(base64.urlsafe_b64decode(request.headers['X-Public-Key'].encode('utf-8')))
+            body = b""
+            async for chunk in resp.body_iterator: # Get response body
+                body += chunk
+            
+            new_key = Fernet.generate_key() # Generate new key
+            encrypted_body = base64.urlsafe_b64encode(Fernet(new_key).encrypt(body)).decode('utf-8') # Encrypt body
+            encrypted_key = base64.urlsafe_b64encode(rsa.encrypt(new_key, client_public_key)).decode('utf-8') # Encrypt Fernet key
+            resp = Response( # Assemble new response object
+                content=json.dumps({
+                    'key': encrypted_key,
+                    'data': encrypted_body
+                }),
+                status_code=resp.status_code,
+                media_type=resp.media_type
+            )
+        resp.headers['X-Public-Key'] = base64.urlsafe_b64encode(PUBLIC_KEY.save_pkcs1()).decode('utf-8') # Set public key header
         return resp
 
-@app.post('/', response_class=JSONResponse)
+@app.post('/', response_class=JSONResponse) # Post @ root, used for testing
 async def post_root(request: Request):
     return JSONResponse({'request_data': request.state.data, 'timestamp': time.ctime()})
 
-@app.get('/', response_class=JSONResponse)
-async def get_root():
-    return JSONResponse({'timestamp': time.ctime()})
+@app.get('/', response_class=JSONResponse) # Get @ root, used for testing and getting the server's public key.
+async def get_root(request: Request):
+    return JSONResponse({'timestamp': time.ctime(), 'parameters': request.query_params._dict})
 
 
 if __name__ == '__main__':
